42382c6f8542b2a3b5bab05ae3c323c4983f8dac
luizfscorreia <luizfscorreia@gmail.com>
2023-04-02 17:36:06 -0300

    Exercices resolutions

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -42,6 +42,10 @@ export function Dashboard() {
      * - call addRepository function sending inputText value;
      * - clean inputText value.
      */
+
+    addRepository(inputText)
+    setInputText('')
+
   }
 
   function handleRepositoryPageNavigation(id: number) {
@@ -53,6 +57,8 @@ export function Dashboard() {
      *  repositoryId: id of the repository
      * })
      */
+
+    navigate('Repository', {repositoryId: id})
   }
 
   return (
@@ -71,6 +77,7 @@ export function Dashboard() {
                * changes:
                * onChangeText={YOUR CODE HERE}
                */
+              onChangeText={text => setInputText(text)}
               onSubmitEditing={handleAddRepository}
               returnKeyType="send"
               autoCapitalize='none'
@@ -85,6 +92,7 @@ export function Dashboard() {
                * empty (use disabled prop to this):
                * disabled={CONDITION HERE}
                */
+              disabled={inputText === ''}
             >
               <Icon name="search" size={20} />
             </InputButton>
